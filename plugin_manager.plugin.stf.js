1e205eb667eb675c611cb80aa1e26fd2b14a2dc3
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-06-04 22:30:16 +0200

    kek

---
+++ plugin_manager.plugin.stf.js
@@ -0,0 +1,30 @@
+// Metadata
+const meta = {
+    id: 'ml.crafterpika.pluginmanager69', // ID's should be a unique string
+    name: 'PluginManager',
+    author: 'CrafterPika',
+    version: '0.0.1',
+    link: 'https://github.com/CrafterPika/starfiles-plugins',
+    compatibility:{
+        website: true
+    }
+}
+// Declare Plugin
+window['start_' + meta.id] = function(){
+    function yeetPlugins() {
+        if (confirm('Are you sure to continue, this will disable all plugins you installed')) {
+            // DO IT!
+            localStorage.removeItem("plugins");
+            alert("Removed all Plugins")
+          } else {
+            alert('cancelled action')
+          }
+    }
+    if (window.location.href == "https://starfiles.co/settings") {
+        var htmltext = '<button class="btn btn-small" onclick="yeetPlugins()">Remove All Plugins</button><br><br>';
+        var x = document.getElementsByClassName('pagecard object'); 
+        x[0].innerHTML = htmltext + x[0].innerHTML;
+    }
+}
+// Run Plugin
+init_plugin(meta);
